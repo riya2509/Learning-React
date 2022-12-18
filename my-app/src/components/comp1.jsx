e0ffd30dba790ca34a6a6957abb61174495afd90
riya2509 <riyamars3@gmail.com>
2022-12-18 23:59:38 +0530

    test

---
+++ my-app/src/components/comp1.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+function Comp1() {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [title, setTitle] = useState("");
+  const [money, setMoney] = useState("");
+  return <div>v</div>;
+}
+export default Comp1;
