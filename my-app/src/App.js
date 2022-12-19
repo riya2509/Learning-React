7e6df2454d32cac07ce2776f21b2e3cbf6385821
riya2509 <riyamars3@gmail.com>
2022-12-19 17:19:53 +0530

    making portfolio

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,27 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-import Variables from "./Variables"
-import Data from "./Data"
-
+import Comp1 from "./components/comp1"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Variables/>
-      <Data/>
+    <div >
+      <Comp1/>
     </div>
   );
 }
