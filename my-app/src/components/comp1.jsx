b26f05866b12bb1bf0ca0b70514ff61a692fdf97
riya2509 <riyamars3@gmail.com>
2022-12-19 14:46:39 +0530

    use of state

--- my-app/src/components/comp1.jsx
+++ my-app/src/components/comp1.jsx
@@ -4,6 +4,50 @@ function Comp1() {
   const [age, setAge] = useState("");
   const [title, setTitle] = useState("");
   const [money, setMoney] = useState("");
-  return <div>v</div>;
+
+  const handleMoney = (event) => setMoney(event.target.value);
+
+  const handleSubmit = () => {
+    console.log(name);
+    console.log(age);
+    console.log(title);
+    console.log(money);
+  };
+  return (
+    <>
+      <input
+        type="text"
+        className="name"
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+        placeholder="Name"
+      />
+      <input
+        type="text"
+        className="age"
+        value={age}
+        onChange={(event) => setAge(event.target.value)}
+        placeholder="Age"
+      />
+
+      <input
+        type="text"
+        className="title"
+        value={title}
+        onChange={(event) => setTitle(event.target.value)}
+        placeholder="Title"
+      />
+
+      <input
+        type="money"
+        className="money"
+        value={money}
+        onChange={handleMoney}
+        placeholder="Money"
+      />
+
+      <button onClick={handleSubmit}>Submit</button>
+    </>
+  );
 }
 export default Comp1;
