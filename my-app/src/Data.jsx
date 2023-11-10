1e108604ba320d1295d3b0890177ecaa6942f69e
riya2509 <91462077+riya2509@users.noreply.github.com>
2023-11-11 00:20:00 +0530

    refactoring

--- my-app/src/Data.jsx
+++ my-app/src/Data.jsx
@@ -1,16 +1,19 @@
-import React from "react";
+// import React from "react";
 function Data() {
-    const personList = [{ name: "Riya", Age: 20 },
+  const personList = [
+    { name: "Riya", Age: 20 },
     { name: "Shalu", Age: 70 },
     { name: "Navya", Age: 25 },
     { name: "Ajay", Age: 28 },
     { name: "Aryan", Age: 15 },
-    { name: "Meenakshi", Age: 45 }
-    ];
-    let arr = personList.name;
-    console.log(personList.map)
-    return (
-        <h2><u>PersonList</u></h2>
-    );
+    { name: "Meenakshi", Age: 45 },
+  ];
+  let arr = personList.name;
+  console.log(personList.map);
+  return (
+    <h2>
+      <u>PersonList</u>
+    </h2>
+  );
 }
 export default Data;
